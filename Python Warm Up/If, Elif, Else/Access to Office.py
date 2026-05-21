98943cc0fe0976291ac5e0e2b33020ed469a64ad
Gustavo Toledo <gtoledodata@gmail.com>
2026-05-21 17:52:08 -0300

    Access to Office file

---
+++ Python Warm Up/If, Elif, Else/Access to Office.py
@@ -0,0 +1,6 @@
+time = input("What time is it? (in 24-hour format, e.g., 14) ")
+
+if 8 < int(time) < 18:
+    print("You have access to the office.")
+else:
+    print("You do not have access to the office.") 
