c6a200097fdeb4235082795c720ec5a97ab4d7dd
Gustavo Toledo <gtoledodata@gmail.com>
2026-05-22 13:28:29 -0300

    new files

---
+++ Python Warm Up/For and While/Login Validation.py
@@ -0,0 +1,16 @@
+while True:
+
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    if len(username) < 5:
+        print("Username must be at least 5 characters long.")
+        continue
+
+    if len(password) < 8:
+        print("Password must be at least 8 characters long.")
+        continue
+
+
+    print("Login successful!")
+    break
