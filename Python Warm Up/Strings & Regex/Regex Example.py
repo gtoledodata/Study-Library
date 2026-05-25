cb8ad243e185beaab89d0fd6bd2ccdf8f65bab47
Gustavo Toledo <gtoledodata@gmail.com>
2026-05-25 17:58:28 -0300

    Regex Example

---
+++ Python Warm Up/Strings & Regex/Regex Example.py
@@ -0,0 +1,12 @@
+import re
+
+text = "Get in touch with us at test@email.com"
+
+email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+
+result = re.search(email_pattern, text)
+
+if result:
+    print("Email found:", result.group())
+else:
+    print("No email found.")
