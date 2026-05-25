e097e653714a7095840f06cc970523d8269f10c6
Gustavo Toledo <gtoledodata@gmail.com>
2026-05-25 18:50:33 -0300

    Validating SSN

---
+++ Python Practice/Strings & Regex/Validating SSN.py
@@ -0,0 +1,14 @@
+def check_SSN():
+    global SSN
+    SSN = input("Enter a Social Security Number (SSN) in the format XXX-XX-XXXX: ")
+
+import re
+
+check_SSN()
+
+while True:
+    if re.fullmatch(r'\d{3}-\d{2}-\d{4}', f'{SSN}'):
+        print("Valid SSN")
+        break
+    print("Invalid SSN, please enter in the format XXX-XX-XXXX.")
+    check_SSN()
